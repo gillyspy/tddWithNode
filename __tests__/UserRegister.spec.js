55958ddc6d9496ab1aff80eacd53219a8d3c8120
gillyspy <gillyspy@gmail.com>
2021-06-07 15:17:57 -0400

    fixed test to wait

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -1,10 +1,13 @@
 const request = require('supertest');
 const app = require('../app');
 
-it('returns 200 OK when signup request is valid', () => {
-  request(app).post('/api/v1/users').send({
-    username: 'user1',
-    email: 'user1@mail.com',
-    password: 'P@sswr0rd',
-  });
+it('returns 200 OK when signup request is valid', (done) => {
+  request(app)
+    .post('/api/v1/users')
+    .send({
+      username: 'user1',
+      email: 'user1@mail.com',
+      password: 'P@sswr0rd',
+    })
+    .expect(200, done);
 });
