81111487842eae68425d04cb5d4ff64e16ea9e32
gillyspy <gillyspy@gmail.com>
2021-06-07 15:24:44 -0400

    gather users test together

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -1,30 +1,32 @@
 const request = require('supertest');
 const app = require('../app');
 
-it('returns 200 OK when signup request is valid', (done) => {
-  request(app)
-    .post('/api/v1/users')
-    .send({
-      username: 'user1',
-      email: 'user1@mail.com',
-      password: 'P@sswr0rd',
-    })
-    .then((response) => {
-      expect(response.status).toBe(200);
-      done();
-    });
-});
+describe('User Registration', () => {
+  it('returns 200 OK when signup request is valid', (done) => {
+    request(app)
+      .post('/api/v1/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P@sswr0rd',
+      })
+      .then((response) => {
+        expect(response.status).toBe(200);
+        done();
+      });
+  });
 
-it('returns success message when signup request is valid', (done) => {
-  request(app)
-    .post('/api/v1/users')
-    .send({
-      username: 'user1',
-      email: 'user1@mail.com',
-      password: 'P@sswr0rd',
-    })
-    .then((response) => {
-      expect(response.body.message).toBe('User Created');
-      done();
-    });
+  it('returns success message when signup request is valid', (done) => {
+    request(app)
+      .post('/api/v1/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P@sswr0rd',
+      })
+      .then((response) => {
+        expect(response.body.message).toBe('User Created');
+        done();
+      });
+  });
 });
