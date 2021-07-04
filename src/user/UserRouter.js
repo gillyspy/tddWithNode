b4bf928cdff00f4d87af9da706eda47055734f0f
gillyspy <gillyspy@gmail.com>
2021-07-04 06:02:30 -0400

    handle null username

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const userService = require('./UserService');
 
 router.post('/', async (req, res) => {
+  const user  = req.body;
+  if( user.username === null){
+    return res.status(400).send();
+  }
+
   await userService.save(req.body);
 
   return res.send({ message: 'User Created' });
