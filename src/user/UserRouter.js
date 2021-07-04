9b12c81012bbb5436f008e795ccd288492f8cdd5
gillyspy <gillyspy@gmail.com>
2021-07-04 14:47:30 -0400

    refactor to use express-validator package in UserRouter

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,30 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const userService = require('./UserService');
+const {check,validationResult} = require('express-validator');
 
-const validateUserName = (req, res, next) => {
-  req.validationErrors = {}
-  if (req.body.username === null)
-    req.validationErrors.username = 'Username cannot be null';
-
-  next();
-}
-
-const validateEmail = (req, res, next) => {
-  if (req.body.email === null)
-    req.validationErrors.email = 'Email cannot be null'
-
-  next();
-}
 
 router.post('/',
-  validateUserName, validateEmail,
+  check('username').notEmpty(),
+    check('email').notEmpty(),
   async (req, res) => {
-    const validationErrors = req.validationErrors;
-    if (Object.keys( validationErrors).length !== 0)
-      return res.status(400).send({
-        validationErrors
-      });
+    const errors = validationResult(req);
+    const validationErrors = {}
+    if(!errors.isEmpty()){
+      errors.array().forEach( error =>{
+        validationErrors[error.param] = error.msg;
+      })
+      return res.status(400).send({validationErrors});
+    }
 
     await userService.save(req.body);
 
